a64d09ceae75ea80bba6ca098996fdd3e879c8c7
Guilherme Pereira <wvggui55@gmail.com>
2026-07-08 15:34:04 -0300

    feat: add `lombok` anotation for abstration on methods `Getter`, `Setter` and on `Constructors`

---
+++ src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -0,0 +1,27 @@
+package com.example.CadastroDeNinjas.Ninjas;
+
+import com.example.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")
+    private MissoesModel missoes;
+}
