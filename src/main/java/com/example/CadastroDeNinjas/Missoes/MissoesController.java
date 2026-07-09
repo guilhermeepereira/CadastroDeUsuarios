19d5ee4290679ceff0470181aa417e368280ed5f
Guilherme Pereira <wvggui55@gmail.com>
2026-07-09 20:59:16 -0300

    feat: creates routes for `MissoesController.java`

--- src/main/java/com/example/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/com/example/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,31 @@
 package com.example.CadastroDeNinjas.Missoes;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Bem vindo a Missões";
+    }
+
+    @PostMapping("/criar")
+    public String criar(){
+        return "Missão criada!";
+    }
+
+    @GetMapping("/listar")
+    public String listar(){
+        return "Missões listadas!";
+    }
+
+    @PutMapping("/atualizar")
+    public String atualizar(){
+        return "Missão atualizada!";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletar(){
+        return "Missão deletada!";
+    }
 }
