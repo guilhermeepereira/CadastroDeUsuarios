b2e8d6700dc419aaea528ce50c5585c09bfb1b17
Guilherme Pereira <wvggui55@gmail.com>
2026-07-09 17:25:30 -0300

    feat: update `NinjaController` - add `Data Mock`

--- src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,11 +1,35 @@
 package com.example.CadastroDeNinjas.Ninjas;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class NinjaController {
     @GetMapping("/welcome")
     public String welcome(){
         return "teste - welcome updated";
+    }
+
+    @PostMapping("/adicionar")
+    public String add(){
+        return "Adicionar Ninja";
+    }
+
+    @GetMapping("/listar")
+    public String listar(){
+        return "lista de ninjas";
+    }
+
+    @GetMapping("/listar_por_id")
+    public String listarID(){
+        return "Lista do ninja com ID";
+    }
+
+    @PutMapping("/atualizar")
+    public String atualizar(){
+        return "Atualizar infos do Ninja";
+    }
+
+    @DeleteMapping("/deletar")
+    public String delete(){
+        return "Deletar Ninja";
     }
 }
