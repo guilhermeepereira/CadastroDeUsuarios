a41450f3fa0884eb0eb03572661e40b3418a5418
Guilherme Pereira <wvggui55@gmail.com>
2026-07-09 21:35:28 -0300

    fix: adds `JsonIgnore` anotation to prevents serialization error

--- src/main/java/com/example/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/example/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,6 +1,7 @@
 package com.example.CadastroDeNinjas.Missoes;
 
 import com.example.CadastroDeNinjas.Ninjas.NinjaModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,6 +23,7 @@ public class MissoesModel {
     private String dificuldade;
 
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 
     @Override
